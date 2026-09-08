2a450a4cb7f34b49b57ec8f2fa1d149c01f5f666
Towhidul Islam <towhidul.islam5797@gmail.com>
2026-09-08 17:19:07 +0600

    New score mechanics and Game end UI added

--- Assets/Scripts/Manager/Gamemanager.cs
+++ Assets/Scripts/Manager/Gamemanager.cs
@@ -2307,6 +2307,188 @@
 #endregion
 
 #region Phase 3 Sprint 3 - GameManager Implementation with Score and Lives, Elapsed Time, Time Penalty for Sandwich, and Time Limit
+//using UnityEngine;
+
+//public class GameManager : MonoBehaviour
+//{
+//    #region Singleton
+//    public static GameManager Instance { get; private set; }
+
+//    private void Awake()
+//    {
+//        Instance = this;
+//    }
+//    #endregion
+
+//    #region Game State
+//    public enum GameState
+//    {
+//        Playing,
+//        Won,
+//        Lost
+//    }
+
+//    private GameState currentState;
+//    public GameState CurrentState => currentState;
+//    public bool IsGameOver => currentState != GameState.Playing;
+//    #endregion
+
+//    #region Lives Settings
+//    [SerializeField] private int startingLives = 3;
+//    private int currentLives;
+//    public int CurrentLives => currentLives;
+//    #endregion
+
+//    #region Score Settings
+//    [SerializeField] private int scorePerMove = 10;
+//    private int currentScore;
+//    public int CurrentScore => currentScore;
+//    #endregion
+
+//    #region Time Settings
+//    [SerializeField] private float parTime = 60f;
+//    [SerializeField] private int scorePenaltyPerSecondOverPar = 1;
+//    [SerializeField] private int scorePenaltyPerLifeLost = 50;
+//    private float elapsedTime;
+//    public float ElapsedTime => elapsedTime;
+//    #endregion
+
+//    #region Time Limit Settings
+//    [SerializeField] private float timeLimit = 90f;
+//    private float remainingTime;
+//    public float RemainingTime => remainingTime;
+//    #endregion
+
+//    #region Unity Lifecycle
+//    private void Start()
+//    {
+//        currentLives = startingLives;
+//        currentScore = 0;
+//        elapsedTime = 0f;
+//        remainingTime = timeLimit;
+//        footballCharges = 0;
+//        currentState = GameState.Playing;
+//    }
+
+//    private void Update()
+//    {
+//        if (currentState == GameState.Playing)
+//        {
+//            elapsedTime += Time.deltaTime;
+//            remainingTime = Mathf.Max(0f, remainingTime - Time.deltaTime);
+
+//            if (remainingTime <= 0f)
+//            {
+//                TimeExpired();
+//            }
+//        }
+//    }
+//    #endregion
+
+//    #region Game State Changes
+//    public void PlayerHitObstacle(int scorePenalty)
+//    {
+//        if (currentState != GameState.Playing) return;
+
+//        currentLives--;
+//        currentScore = Mathf.Max(0, currentScore - scorePenalty);
+//        Debug.Log("Life lost. Lives remaining: " + currentLives);
+
+//        if (currentLives <= 0)
+//        {
+//            currentState = GameState.Lost;
+//            AudioManager.Instance.PlayDeath();
+//            Debug.Log("Game Over");
+//        }
+//        else
+//        {
+//            AudioManager.Instance.PlayHit();
+//        }
+//    }
+
+//    public void PlayerHitSandwich(int scorePenalty, float timePenaltySeconds)
+//    {
+//        if (currentState != GameState.Playing) return;
+
+//        currentScore = Mathf.Max(0, currentScore - scorePenalty);
+//        remainingTime = Mathf.Max(0f, remainingTime - timePenaltySeconds);
+//        AudioManager.Instance.PlayHit();
+//    }
+
+//    public void PlayerReachedGoal()
+//    {
+//        if (currentState != GameState.Playing) return;
+
+//        int triesUsed = startingLives - currentLives;
+//        float secondsOverPar = Mathf.Max(0f, elapsedTime - parTime);
+
+//        int triesPenalty = triesUsed * scorePenaltyPerLifeLost;
+//        int timePenalty = Mathf.RoundToInt(secondsOverPar * scorePenaltyPerSecondOverPar);
+
+//        currentScore = Mathf.Max(0, currentScore - triesPenalty - timePenalty);
+
+//        currentState = GameState.Won;
+//        Debug.Log("You Win. Tries used: " + triesUsed + ", Seconds over par: " + secondsOverPar);
+//    }
+
+//    public void AddMoveScore()
+//    {
+//        if (currentState != GameState.Playing) return;
+
+//        currentScore += scorePerMove;
+//    }
+
+//    private void TimeExpired()
+//    {
+//        if (currentState != GameState.Playing) return;
+
+//        currentState = GameState.Lost;
+//        Debug.Log("Time's up. Game Over");
+//    }
+//    #endregion
+
+//    #region Football Charges
+//    [SerializeField] private int maxFootballCharges = 3;
+//    private int footballCharges;
+//    public int FootballCharges => footballCharges;
+
+//    public void CollectFootball()
+//    {
+//        footballCharges = Mathf.Min(footballCharges + 1, maxFootballCharges);
+//        AudioManager.Instance.PlayPickup();
+//    }
+
+//    public bool SpendFootball()
+//    {
+//        if (footballCharges <= 0) return false;
+
+//        footballCharges--;
+//        return true;
+//    }
+//    #endregion
+
+//    #region Quarterzip Pickup
+//    public void CollectQuarterzip(int bonusScore, float bonusTimeSeconds)
+//    {
+//        if (currentState != GameState.Playing) return;
+
+//        currentScore += bonusScore;
+//        remainingTime += bonusTimeSeconds;
+
+//        if (bonusTimeSeconds > 0f)
+//        {
+//            AudioManager.Instance.PlayBonusScore();
+//        }
+//        else
+//        {
+//            AudioManager.Instance.PlayPickup();
+//        }
+//    }
+//    #endregion
+//}
+#endregion
+
+#region Phase 3 Sprint 3 - GameManager Implementation with Score and Lives, Elapsed Time, Time Penalty for Sandwich, and Time Limit
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -2340,14 +2522,12 @@ public class GameManager : MonoBehaviour
     #endregion
 
     #region Score Settings
-    [SerializeField] private int scorePerMove = 10;
     private int currentScore;
     public int CurrentScore => currentScore;
     #endregion
 
     #region Time Settings
-    [SerializeField] private float parTime = 60f;
-    [SerializeField] private int scorePenaltyPerSecondOverPar = 1;
+    [SerializeField] private int pointsPerSecondRemaining = 10;
     [SerializeField] private int scorePenaltyPerLifeLost = 50;
     private float elapsedTime;
     public float ElapsedTime => elapsedTime;
@@ -2420,22 +2600,15 @@ public class GameManager : MonoBehaviour
         if (currentState != GameState.Playing) return;
 
         int triesUsed = startingLives - currentLives;
-        float secondsOverPar = Mathf.Max(0f, elapsedTime - parTime);
+        float timeRemaining = Mathf.Max(0f, timeLimit - elapsedTime);
 
         int triesPenalty = triesUsed * scorePenaltyPerLifeLost;
-        int timePenalty = Mathf.RoundToInt(secondsOverPar * scorePenaltyPerSecondOverPar);
+        int timeBonus = Mathf.RoundToInt(timeRemaining * pointsPerSecondRemaining);
 
-        currentScore = Mathf.Max(0, currentScore - triesPenalty - timePenalty);
+        currentScore = Mathf.Max(0, currentScore + timeBonus - triesPenalty);
 
         currentState = GameState.Won;
-        Debug.Log("You Win. Tries used: " + triesUsed + ", Seconds over par: " + secondsOverPar);
-    }
-
-    public void AddMoveScore()
-    {
-        if (currentState != GameState.Playing) return;
-
-        currentScore += scorePerMove;
+        Debug.Log("You Win. Tries used: " + triesUsed + ", Time remaining: " + timeRemaining);
     }
 
     private void TimeExpired()
--- Assets/Scripts/UI/GameOverUI.cs
+++ Assets/Scripts/UI/GameOverUI.cs
@@ -7,45 +7,146 @@
 #endregion
 
 #region Phase 2 Sprint 8 - Game Over UI
-using TMPro;
+//using TMPro;
+//using UnityEngine;
+
+//public class GameOverUI : MonoBehaviour
+//{
+//    #region UI References
+//    [SerializeField] private GameObject gameOverPanel;
+//    [SerializeField] private TMP_Text messageText;
+//    #endregion
+
+//    #region Settings
+//    [SerializeField] private string winMessage = "You Win!";
+//    [SerializeField] private string loseMessage = "Game Over";
+//    #endregion
+
+//    #region Unity Lifecycle
+//    private void Awake()
+//    {
+//        gameOverPanel.SetActive(false);
+//    }
+
+//    private void Update()
+//    {
+//        if (GameManager.Instance.CurrentState == GameManager.GameState.Won)
+//        {
+//            ShowPanel(winMessage);
+//        }
+//        else if (GameManager.Instance.CurrentState == GameManager.GameState.Lost)
+//        {
+//            ShowPanel(loseMessage);
+//        }
+//    }
+//    #endregion
+
+//    #region Panel Display
+//    private void ShowPanel(string message)
+//    {
+//        gameOverPanel.SetActive(true);
+//        messageText.text = message;
+//    }
+//    #endregion
+//}
+#endregion
+
+#region Phase 3 Sprint 8 - Game Over UI with Win Lose Art
+//using UnityEngine;
+//using UnityEngine.UI;
+
+//public class GameOverUI : MonoBehaviour
+//{
+//    #region UI References
+//    [SerializeField] private GameObject gameOverPanel;
+//    [SerializeField] private Image resultImage;
+//    #endregion
+
+//    #region Result Art
+//    [SerializeField] private Sprite winSprite;
+//    [SerializeField] private Sprite loseSprite;
+//    #endregion
+
+//    #region Private State
+//    private bool hasShownPanel;
+//    #endregion
+
+//    #region Unity Lifecycle
+//    private void Awake()
+//    {
+//        gameOverPanel.SetActive(false);
+//    }
+
+//    private void Update()
+//    {
+//        if (hasShownPanel) return;
+
+//        if (GameManager.Instance.CurrentState == GameManager.GameState.Won)
+//        {
+//            ShowPanel(winSprite);
+//        }
+//        else if (GameManager.Instance.CurrentState == GameManager.GameState.Lost)
+//        {
+//            ShowPanel(loseSprite);
+//        }
+//    }
+//    #endregion
+
+//    #region Panel Display
+//    private void ShowPanel(Sprite artwork)
+//    {
+//        hasShownPanel = true;
+//        resultImage.sprite = artwork;
+//        gameOverPanel.SetActive(true);
+//    }
+//    #endregion
+//}
+#endregion
+
+#region Phase 3 Sprint 8 - Game Over UI with Win Lose Art
 using UnityEngine;
 
 public class GameOverUI : MonoBehaviour
 {
     #region UI References
     [SerializeField] private GameObject gameOverPanel;
-    [SerializeField] private TMP_Text messageText;
+    [SerializeField] private GameObject winScreen;
+    [SerializeField] private GameObject loseScreen;
     #endregion
 
-    #region Settings
-    [SerializeField] private string winMessage = "You Win!";
-    [SerializeField] private string loseMessage = "Game Over";
+    #region Private State
+    private bool hasShownPanel;
     #endregion
 
     #region Unity Lifecycle
     private void Awake()
     {
         gameOverPanel.SetActive(false);
+        winScreen.SetActive(false);
+        loseScreen.SetActive(false);
     }
 
     private void Update()
     {
+        if (hasShownPanel) return;
+
         if (GameManager.Instance.CurrentState == GameManager.GameState.Won)
         {
-            ShowPanel(winMessage);
+            ShowPanel(winScreen);
         }
         else if (GameManager.Instance.CurrentState == GameManager.GameState.Lost)
         {
-            ShowPanel(loseMessage);
+            ShowPanel(loseScreen);
         }
     }
     #endregion
 
     #region Panel Display
-    private void ShowPanel(string message)
+    private void ShowPanel(GameObject screen)
     {
+        hasShownPanel = true;
+        screen.SetActive(true);
         gameOverPanel.SetActive(true);
-        messageText.text = message;
     }
     #endregion
 }
