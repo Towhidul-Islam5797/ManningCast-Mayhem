3dba971d90c3f36b1ee372d8e97e625982e1f4a6
Towhidul Islam <towhidul.islam5797@gmail.com>
2026-09-17 12:55:20 +0600

    New Audio Added

--- Assets/Scripts/Manager/Core/AudioManager.cs
+++ Assets/Scripts/Manager/Core/AudioManager.cs
@@ -93,6 +93,173 @@
 #endregion
 
 #region Phase 3 Sprint 7 - Audio Manager
+//using UnityEngine;
+
+//public class AudioManager : MonoBehaviour
+//{
+//    public static AudioManager Instance { get; private set; }
+
+//    #region Audio Sources
+//    [SerializeField] private AudioSource musicSource;
+//    [SerializeField] private AudioSource sfxSource;
+//    #endregion
+
+//    #region Clips
+//    [SerializeField] private AudioClip backgroundMusic;
+//    [SerializeField] private AudioClip moveClip;
+//    [SerializeField] private AudioClip pickupClip;
+//    [SerializeField] private AudioClip bonusScoreClip;
+//    [SerializeField] private AudioClip hitClip;
+//    [SerializeField] private AudioClip deathClip;
+//    #endregion
+
+//    #region Unity Lifecycle
+//    private void Awake()
+//    {
+//        if (Instance != null && Instance != this)
+//        {
+//            Destroy(gameObject);
+//            return;
+//        }
+
+//        Instance = this;
+//        DontDestroyOnLoad(gameObject);
+//    }
+
+//    private void Start()
+//    {
+//        if (backgroundMusic == null) return;
+
+//        musicSource.clip = backgroundMusic;
+//        musicSource.loop = true;
+//        musicSource.Play();
+//    }
+//    #endregion
+
+//    #region Sound Effects
+//    public void PlayMove()
+//    {
+//        sfxSource.PlayOneShot(moveClip);
+//    }
+
+//    public void PlayPickup()
+//    {
+//        sfxSource.PlayOneShot(pickupClip);
+//    }
+
+//    public void PlayBonusScore()
+//    {
+//        sfxSource.PlayOneShot(bonusScoreClip);
+//    }
+
+//    public void PlayHit()
+//    {
+//        sfxSource.PlayOneShot(hitClip);
+//    }
+
+//    public void PlayDeath()
+//    {
+//        sfxSource.PlayOneShot(deathClip);
+//    }
+//    #endregion
+
+//    #region Volume Controls
+//    public void SetMusicVolume(float volume)
+//    {
+//        musicSource.volume = volume;
+//    }
+
+//    public void SetSfxVolume(float volume)
+//    {
+//        sfxSource.volume = volume;
+//    }
+//    #endregion
+//}
+//using UnityEngine;
+
+//public class AudioManager : MonoBehaviour
+//{
+//    public static AudioManager Instance { get; private set; }
+
+//    #region Audio Sources
+//    [SerializeField] private AudioSource musicSource;
+//    [SerializeField] private AudioSource sfxSource;
+//    #endregion
+
+//    #region Clips
+//    [SerializeField] private AudioClip backgroundMusic;
+//    [SerializeField] private AudioClip moveClip;
+//    [SerializeField] private AudioClip pickupClip;
+//    [SerializeField] private AudioClip bonusScoreClip;
+//    [SerializeField] private AudioClip hitClip;
+//    [SerializeField] private AudioClip deathClip;
+//    #endregion
+
+//    #region Unity Lifecycle
+//    private void Awake()
+//    {
+//        if (Instance != null && Instance != this)
+//        {
+//            Destroy(gameObject);
+//            return;
+//        }
+
+//        Instance = this;
+//        DontDestroyOnLoad(gameObject);
+//    }
+
+//    private void Start()
+//    {
+//        if (backgroundMusic == null) return;
+
+//        musicSource.clip = backgroundMusic;
+//        musicSource.loop = true;
+//        musicSource.Play();
+//    }
+//    #endregion
+
+//    #region Sound Effects
+//    public void PlayMove()
+//    {
+//        sfxSource.PlayOneShot(moveClip);
+//    }
+
+//    public void PlayPickup()
+//    {
+//        sfxSource.PlayOneShot(pickupClip);
+//    }
+
+//    public void PlayBonusScore()
+//    {
+//        sfxSource.PlayOneShot(bonusScoreClip);
+//    }
+
+//    public void PlayHit()
+//    {
+//        sfxSource.PlayOneShot(hitClip);
+//    }
+
+//    public void PlayDeath()
+//    {
+//        sfxSource.PlayOneShot(deathClip);
+//    }
+//    #endregion
+
+//    #region Volume Controls
+//    public void SetMusicVolume(float volume)
+//    {
+//        musicSource.volume = volume;
+//    }
+
+//    public void SetSfxVolume(float volume)
+//    {
+//        sfxSource.volume = volume;
+//    }
+//    #endregion
+//}
+#endregion
+
+#region Phase 4 Sprint 1 - Audio Manager Expanded Clips
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
@@ -111,6 +278,11 @@ public class AudioManager : MonoBehaviour
     [SerializeField] private AudioClip bonusScoreClip;
     [SerializeField] private AudioClip hitClip;
     [SerializeField] private AudioClip deathClip;
+    [SerializeField] private AudioClip winClip;
+    [SerializeField] private AudioClip loseClip;
+    [SerializeField] private AudioClip lostLifeClip;
+    [SerializeField] private AudioClip quarterzipClip;
+    [SerializeField] private AudioClip footballThrowClip;
     #endregion
 
     #region Unity Lifecycle
@@ -160,6 +332,31 @@ public class AudioManager : MonoBehaviour
     public void PlayDeath()
     {
         sfxSource.PlayOneShot(deathClip);
+    }
+
+    public void PlayWin()
+    {
+        sfxSource.PlayOneShot(winClip);
+    }
+
+    public void PlayLose()
+    {
+        sfxSource.PlayOneShot(loseClip);
+    }
+
+    public void PlayLostLife()
+    {
+        sfxSource.PlayOneShot(lostLifeClip);
+    }
+
+    public void PlayQuarterzip()
+    {
+        sfxSource.PlayOneShot(quarterzipClip);
+    }
+
+    public void PlayFootballThrow()
+    {
+        sfxSource.PlayOneShot(footballThrowClip);
     }
     #endregion
 
